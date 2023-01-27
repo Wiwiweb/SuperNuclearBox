c8e1336c88950926fac29db3f48ec43c4052f950
Wiwiweb <wiwiweb2@gmail.com>
2023-01-26 18:05:23 -0800

    Display walls from level data

--- Assets/Scripts/LevelCreator.cs
+++ Assets/Scripts/LevelCreator.cs
@@ -12,9 +12,9 @@ public class LevelCreator : MonoBehaviour
 
   private int floorGridSize = 100;
 
-  enum TileType
+  public enum TileType
   {
-    Ground,
+    Floor,
     Wall,
   }
 
@@ -33,7 +33,7 @@ public class LevelCreator : MonoBehaviour
   private TileWithProbability[] floorTiles;
   private TileWithProbability[] wallTiles;
   private TileWithProbability[] wallDecorationsBottomTiles;
-  private TileWithProbability[] wallDecorationsUpTiles;
+  private TileWithProbability[] wallDecorationsTopTiles;
   private TileWithProbability[] wallDecorationsDownTiles;
   private TileWithProbability[] wallDecorationsLeftTiles;
   private TileWithProbability[] wallDecorationsRightTiles;
@@ -42,20 +42,25 @@ public class LevelCreator : MonoBehaviour
   private TileWithProbability[] wallDecorationsCornerBottomLeftTiles;
   private TileWithProbability[] wallDecorationsCornerBottomRightTiles;
 
-  int[,] level_int = { 
-    { 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 }, 
-    { 1, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 1 }, 
-    { 1, 1, 0, 0, 1, 0, 0, 1, 1, 1, 0, 1 }, 
-    { 1, 1, 0, 0, 1, 0, 0, 1, 1, 1, 0, 1 }, 
-    { 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1 }, 
-    { 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1 }, 
-    { 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1 }, 
-    { 1, 1, 0, 0, 1, 1, 1, 0, 0, 0, 0, 1 }, 
-    { 1, 1, 0, 1, 1, 1, 0, 0, 0, 0, 0, 1 }, 
-    { 1, 1, 0, 1, 1, 1, 0, 0, 0, 0, 0, 1 }, 
-    { 1, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1 }, 
-    { 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 }, 
+  private int[,] level_int = {
+    { 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 },
+    { 1, 1, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 1 },
+    { 1, 1, 0, 0, 0, 1, 0, 0, 1, 1, 1, 0, 1 },
+    { 1, 1, 0, 0, 0, 1, 0, 0, 1, 1, 1, 0, 1 },
+    { 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1 },
+    { 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1 },
+    { 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1 },
+    { 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1 },
+    { 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1 },
+    { 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1 },
+    { 1, 1, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 1 },
+    { 1, 1, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 1 },
+    { 1, 1, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 1 },
+    { 1, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1 },
+    { 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 },
   };
+
+  private TileType[,] level;
 
   // Start is called before the first frame update
   void Start()
@@ -65,6 +70,22 @@ public class LevelCreator : MonoBehaviour
     wallDecorationsUpDownCornersTilemap = GameObject.Find("Tilemap_WallDecorations_UpDownCorners").GetComponent<Tilemap>();
     wallDecorationsSidesTilemap = GameObject.Find("Tilemap_Floors").GetComponent<Tilemap>();
 
+    level = new TileType[level_int.GetLength(1), level_int.GetLength(0)];
+    for (int x = 0; x <= level.GetUpperBound(0); x++)
+    {
+      for (int y = 0; y <= level.GetUpperBound(1); y++)
+      {
+        if (level_int[y, x] == 0)
+        {
+          level[x, level.GetUpperBound(1)-y] = TileType.Floor;
+        }
+        else
+        {
+          level[x, level.GetUpperBound(1)-y] = TileType.Wall;
+        }
+      }
+    }
+
     InitializeTileArrays();
 
     FillLevelTilemaps();
@@ -73,12 +94,21 @@ public class LevelCreator : MonoBehaviour
   // Update is called once per frame
   void Update()
   {
-    
+
   }
 
   private void InitializeTileArrays()
   {
-    floorTiles = InitializeTileArray("Floor tiles/Desert floor", new float[] {0.335f, 0.33f, 0.33f, 0.005f});
+    floorTiles = InitializeTileArray("Floor tiles/Desert floor", new float[] { 0.335f, 0.33f, 0.33f, 0.005f });
+    wallTiles = InitializeTileArray("Wall tiles/Desert wall", new float[] { 0.25f, 0.25f, 0.25f, 0.25f });
+    wallDecorationsBottomTiles = InitializeTileArray("Wall decorations tiles/Desert wall decoration bottom", new float[] { 0.25f, 0.25f, 0.25f, 0.25f });
+    wallDecorationsTopTiles = InitializeTileArray("Wall decorations tiles/Desert wall decoration top", new float[] { 1f });
+    wallDecorationsLeftTiles = InitializeTileArray("Wall decorations tiles/Desert wall decoration left", new float[] { 1f });
+    wallDecorationsRightTiles = InitializeTileArray("Wall decorations tiles/Desert wall decoration right", new float[] { 1f });
+    wallDecorationsCornerTopLeftTiles = InitializeTileArray("Wall decorations tiles/Desert wall decoration corner top left", new float[] { 1f });
+    wallDecorationsCornerTopRightTiles = InitializeTileArray("Wall decorations tiles/Desert wall decoration corner top right", new float[] { 1f });
+    // wallDecorationsCornerBottomLeftTiles = InitializeTileArray("Wall decorations tiles/Desert wall decoration corner bottom left", new float[] {1f});
+    wallDecorationsCornerBottomRightTiles = InitializeTileArray("Wall decorations tiles/Desert wall decoration corner bottom right", new float[] { 1f });
   }
 
   private TileWithProbability[] InitializeTileArray(string tilePrefix, float[] probabilities)
@@ -96,13 +126,26 @@ public class LevelCreator : MonoBehaviour
 
   private void FillLevelTilemaps()
   {
-    Debug.Log(floorTilemap.cellBounds.min);
-    Debug.Log(floorTilemap.cellBounds.max);
-    for (int x = -floorGridSize; x < floorGridSize; x++)
+    // Floors
+    for (int x = 0; x < floorGridSize; x++)
     {
-      for (int y = -floorGridSize; y < floorGridSize; y++)
+      for (int y = 0; y < floorGridSize; y++)
       {
         floorTilemap.SetTile(new Vector3Int(x, y, 0), getRandomTile(floorTiles));
+      }
+    }
+
+    // Walls
+    int levelOffsetX = floorGridSize - (level.GetLength(0) / 2);
+    int levelOffsetY = floorGridSize - (level.GetLength(1) / 2);
+    for (int x = 0; x <= level.GetUpperBound(0); x++)
+    {
+      for (int y = 0; y <= level.GetUpperBound(1); y++)
+      {
+        if (level[x,y] == TileType.Wall)
+        {
+          wallTilemap.SetTile(new Vector3Int(x + levelOffsetX, y + levelOffsetY, 0), getRandomTile(wallTiles));
+        }     
       }
     }
   }
@@ -113,7 +156,8 @@ public class LevelCreator : MonoBehaviour
     for (int i = 0; i < tileArray.Length - 1; i++)
     {
       TileWithProbability tileWithProbability = tileArray[i];
-      if (rand <= tileWithProbability.probability) {
+      if (rand <= tileWithProbability.probability)
+      {
         return tileWithProbability.tile;
       }
       rand -= tileWithProbability.probability;
