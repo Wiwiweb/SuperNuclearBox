9f81dfe03bddeba71080009f23cf8daf214ac368
Wiwiweb <wiwiweb2@gmail.com>
2023-01-29 19:19:17 -0800

    Wall decorations generation

--- Assets/Scripts/LevelCreator.cs
+++ Assets/Scripts/LevelCreator.cs
@@ -7,8 +7,14 @@ public class LevelCreator : MonoBehaviour
 {
   private Tilemap floorTilemap;
   private Tilemap wallTilemap;
-  private Tilemap wallDecorationsUpDownCornersTilemap;
-  private Tilemap wallDecorationsSidesTilemap;
+  private Tilemap wallDecorationTopTilemap;
+  private Tilemap wallDecorationBottomTilemap;
+  private Tilemap wallDecorationLeftTilemap;
+  private Tilemap wallDecorationRightTilemap;
+  private Tilemap wallDecorationCornerTopLeftTilemap;
+  private Tilemap wallDecorationCornerTopRightTilemap;
+  private Tilemap wallDecorationCornerBottomLeftTilemap;
+  private Tilemap wallDecorationCornerBottomRightTilemap;
 
   private int floorGridSize = 100;
 
@@ -20,15 +26,14 @@ public class LevelCreator : MonoBehaviour
 
   private TileBase floorTile;
   private TileBase wallTile;
-  private TileBase wallDecorationsBottomTiles;
-  private TileBase wallDecorationsTopTiles;
-  private TileBase wallDecorationsDownTiles;
-  private TileBase wallDecorationsLeftTiles;
-  private TileBase wallDecorationsRightTiles;
-  private TileBase wallDecorationsCornerTopLeftTiles;
-  private TileBase wallDecorationsCornerTopRightTiles;
-  private TileBase wallDecorationsCornerBottomLeftTiles;
-  private TileBase wallDecorationsCornerBottomRightTiles;
+  private TileBase wallDecorationsBottomTile;
+  private TileBase wallDecorationsTopTile;
+  private TileBase wallDecorationsLeftTile;
+  private TileBase wallDecorationsRightTile;
+  private TileBase wallDecorationsCornerTopLeftTile;
+  private TileBase wallDecorationsCornerTopRightTile;
+  private TileBase wallDecorationsCornerBottomLeftTile;
+  private TileBase wallDecorationsCornerBottomRightTile;
 
   public GameObject playerPrefabTemp;
   public GameObject cameraTemp;
@@ -58,8 +63,14 @@ public class LevelCreator : MonoBehaviour
   {
     floorTilemap = GameObject.Find("Tilemap_Floors").GetComponent<Tilemap>();
     wallTilemap = GameObject.Find("Tilemap_Walls").GetComponent<Tilemap>();
-    wallDecorationsUpDownCornersTilemap = GameObject.Find("Tilemap_WallDecorations_UpDownCorners").GetComponent<Tilemap>();
-    wallDecorationsSidesTilemap = GameObject.Find("Tilemap_Floors").GetComponent<Tilemap>();
+    wallDecorationTopTilemap = GameObject.Find("Tilemap_WallDecorations_Top").GetComponent<Tilemap>();
+    wallDecorationBottomTilemap = GameObject.Find("Tilemap_WallDecorations_Bottom").GetComponent<Tilemap>();
+    wallDecorationLeftTilemap = GameObject.Find("Tilemap_WallDecorations_Left").GetComponent<Tilemap>();
+    wallDecorationRightTilemap = GameObject.Find("Tilemap_WallDecorations_Right").GetComponent<Tilemap>();
+    wallDecorationCornerTopLeftTilemap = GameObject.Find("Tilemap_WallDecorations_Corner_Top_Left").GetComponent<Tilemap>();
+    wallDecorationCornerTopRightTilemap = GameObject.Find("Tilemap_WallDecorations_Corner_Top_Right").GetComponent<Tilemap>();
+    wallDecorationCornerBottomLeftTilemap = GameObject.Find("Tilemap_WallDecorations_Corner_Bottom_Left").GetComponent<Tilemap>();
+    wallDecorationCornerBottomRightTilemap = GameObject.Find("Tilemap_WallDecorations_Corner_Bottom_Right").GetComponent<Tilemap>();
 
     level = new TileType[level_int.GetLength(1), level_int.GetLength(0)];
     for (int x = 0; x <= level.GetUpperBound(0); x++)
@@ -68,11 +79,11 @@ public class LevelCreator : MonoBehaviour
       {
         if (level_int[y, x] == 0)
         {
-          level[x, level.GetUpperBound(1)-y] = TileType.Floor;
+          level[x, level.GetUpperBound(1) - y] = TileType.Floor;
         }
         else
         {
-          level[x, level.GetUpperBound(1)-y] = TileType.Wall;
+          level[x, level.GetUpperBound(1) - y] = TileType.Wall;
         }
       }
     }
@@ -81,7 +92,7 @@ public class LevelCreator : MonoBehaviour
 
     FillLevelTilemaps();
 
-    GameObject player = Instantiate(playerPrefabTemp, new Vector3(17,16,1), Quaternion.identity);
+    GameObject player = Instantiate(playerPrefabTemp, new Vector3(16, 16, 1), Quaternion.identity);
     FollowPlayer cameraScript = cameraTemp.GetComponent<FollowPlayer>();
     cameraScript.player = player;
   }
@@ -96,20 +107,21 @@ public class LevelCreator : MonoBehaviour
   {
     floorTile = Resources.Load<TileBase>("Tiles/Floor tiles/Desert floor");
     wallTile = Resources.Load<TileBase>("Tiles/Wall tiles/Desert wall");
-    wallDecorationsBottomTiles = Resources.Load<TileBase>("Tiles/Wall decorations tiles/Desert wall decoration bottom");
-    wallDecorationsTopTiles = Resources.Load<TileBase>("Tiles/Wall decorations tiles/Desert wall decoration top");
-    wallDecorationsLeftTiles = Resources.Load<TileBase>("Tiles/Wall decorations tiles/Desert wall decoration left");
-    wallDecorationsRightTiles = Resources.Load<TileBase>("Tiles/Wall decorations tiles/Desert wall decoration right");
-    wallDecorationsCornerTopLeftTiles = Resources.Load<TileBase>("Tiles/Wall decorations tiles/Desert wall decoration corner top left");
-    wallDecorationsCornerTopRightTiles = Resources.Load<TileBase>("Tiles/Wall decorations tiles/Desert wall decoration corner top right");
-    // wallDecorationsCornerBottomLeftTiles = InitializeTileArray("Wall decorations tiles/Desert wall decoration corner bottom left", new float[] {1f});
-    wallDecorationsCornerBottomRightTiles = Resources.Load<TileBase>("Tiles/Wall decorations tiles/Desert wall decoration corner bottom right");
+    wallDecorationsBottomTile = Resources.Load<TileBase>("Tiles/Wall decorations tiles/Desert wall decoration bottom");
+    wallDecorationsTopTile = Resources.Load<TileBase>("Tiles/Wall decorations tiles/Desert wall decoration top");
+    wallDecorationsLeftTile = Resources.Load<TileBase>("Tiles/Wall decorations tiles/Desert wall decoration left");
+    wallDecorationsRightTile = Resources.Load<TileBase>("Tiles/Wall decorations tiles/Desert wall decoration right");
+    wallDecorationsCornerTopLeftTile = Resources.Load<TileBase>("Tiles/Wall decorations tiles/Desert wall decoration corner top left");
+    // wallDecorationsCornerTopRightTile = Resources.Load<TileBase>("Tiles/Wall decorations tiles/Desert wall decoration corner top right");
+    wallDecorationsCornerTopRightTile = null;
+    wallDecorationsCornerBottomLeftTile = Resources.Load<TileBase>("Wall decorations tiles/Desert wall decoration corner bottom left");
+    wallDecorationsCornerBottomRightTile = Resources.Load<TileBase>("Tiles/Wall decorations tiles/Desert wall decoration corner bottom right");
   }
 
   private void FillLevelTilemaps()
   {
     floorTilemap.size = new Vector3Int(floorGridSize, floorGridSize, 1);
-    wallTilemap.size = new Vector3Int(floorGridSize*2, floorGridSize*2, 1);
+    wallTilemap.size = new Vector3Int(floorGridSize * 2, floorGridSize * 2, 1);
 
     // Floors
     floorTilemap.FloodFill(new Vector3Int(0, 0, 0), floorTile);
@@ -121,12 +133,44 @@ public class LevelCreator : MonoBehaviour
     {
       for (int y = 0; y <= level.GetUpperBound(1); y++)
       {
-        if (level[x,y] == TileType.Wall)
+        int tilemapX = x + levelOffsetX;
+        int tilemapY = y + levelOffsetY;
+        if (level[x, y] == TileType.Wall)
         {
-          wallTilemap.SetTile(new Vector3Int(x + levelOffsetX, y + levelOffsetY, 0), wallTile);
-        }     
+          wallTilemap.SetTile(new Vector3Int(tilemapX, tilemapY, 0), wallTile);
+
+          // Wall decorations
+          checkAndPlaceWallDecorationWithCorners(x, y + 1, tilemapX, tilemapY + 1, wallDecorationTopTilemap, wallDecorationsTopTile, wallDecorationCornerTopLeftTilemap, wallDecorationCornerTopRightTilemap, wallDecorationsCornerTopLeftTile, wallDecorationsCornerTopRightTile);
+          checkAndPlaceWallDecorationWithCorners(x, y - 1, tilemapX, tilemapY - 1, wallDecorationBottomTilemap, wallDecorationsBottomTile, wallDecorationCornerBottomLeftTilemap, wallDecorationCornerBottomRightTilemap, wallDecorationsCornerBottomLeftTile, wallDecorationsCornerBottomRightTile);
+          checkAndPlaceWallDecoration(x - 1, y, tilemapX - 1, tilemapY, wallDecorationLeftTilemap, wallDecorationsLeftTile);
+          checkAndPlaceWallDecoration(x + 1, y, tilemapX + 1, tilemapY, wallDecorationRightTilemap, wallDecorationsRightTile);
+        }
       }
     }
     wallTilemap.FloodFill(new Vector3Int(0, 0, 0), wallTile);
+  }
+
+  private void checkAndPlaceWallDecoration(int x, int y, int tilemapX, int tilemapY, Tilemap tilemap, TileBase tile)
+  {
+    if (0 <= x && x <= level.GetUpperBound(0) && 0 <= y && y <= level.GetUpperBound(1) && level[x, y] == TileType.Floor)
+    {
+      tilemap.SetTile(new Vector3Int(tilemapX, tilemapY, 0), tile);
+    }
+  }
+
+  private void checkAndPlaceWallDecorationWithCorners(int x, int y, int tilemapX, int tilemapY, Tilemap tilemap, TileBase tile, Tilemap tilemapCornerLeft, Tilemap tilemapCornerRight, TileBase tileCornerLeft, TileBase tileCornerRight)
+  {
+    if (0 <= x && x <= level.GetUpperBound(0) && 0 <= y && y <= level.GetUpperBound(1) && level[x, y] == TileType.Floor)
+    {
+      tilemap.SetTile(new Vector3Int(tilemapX, tilemapY, 0), tile);
+      if (0 <= x - 1 && level[x - 1, y] == TileType.Floor && level[x - 1, y - 1] == TileType.Floor)
+      {
+        tilemapCornerLeft.SetTile(new Vector3Int(tilemapX - 1, tilemapY, 0), tileCornerLeft);
+      }
+      if (x + 1 <= level.GetUpperBound(0) && level[x + 1, y] == TileType.Floor && level[x + 1, y - 1] == TileType.Floor)
+      {
+        tilemapCornerRight.SetTile(new Vector3Int(tilemapX + 1, tilemapY, 0), tileCornerRight);
+      }
+    }
   }
 }
