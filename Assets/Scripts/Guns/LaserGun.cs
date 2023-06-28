7f020a20205bc934a6e8246d86c97b36902e7392
Wiwiweb <wiwiweb2@gmail.com>
2023-06-27 21:13:30 -0700

    Laser gun animation and spread tweaks

--- Assets/Scripts/Guns/LaserGun.cs
+++ Assets/Scripts/Guns/LaserGun.cs
@@ -7,7 +7,7 @@ public class LaserGun : AbstractSemiAutoGun
   [field: SerializeField]
   protected override float Cooldown { get; set; } = 0.11f;
   [field: SerializeField]
-  protected override float RandomSpread { get; set; } = 10;
+  protected override float RandomSpread { get; set; } = 5;
   [field: SerializeField]
   protected override float CameraKickback { get; set; } = 0;
 
