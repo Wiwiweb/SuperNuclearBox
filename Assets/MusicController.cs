232f7762d2ab88d3069e57b196c8094b153c4542
Wiwiweb <wiwiweb2@gmail.com>
2023-07-09 20:45:02 -0700

    Shorten music loop so it doesnt sound bad on WebGL

--- Assets/MusicController.cs
+++ Assets/MusicController.cs
@@ -26,6 +26,13 @@ public class MusicController : MonoBehaviour
   {
     double startTime = AudioSettings.dspTime + 0.5;
     double introLength = (double)audioSourceIntro.clip.samples / audioSourceIntro.clip.frequency;
+
+    #if UNITY_WEBGL
+      // The loop with intro doesn't work very precisely with the WebGL audio system...
+      // Overlap them a bit, still sounds ok
+      introLength -= 0.1;
+    #endif
+    
     audioSourceIntro.PlayScheduled(startTime);
     audioSourceLoop.PlayScheduled(startTime + introLength);
   }
